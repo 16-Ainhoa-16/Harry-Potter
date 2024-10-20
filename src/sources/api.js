cc7267a7b26e4005fc10e3a29d606edb7758c5eb
16-Ainhoa-16 <117347227+16-Ainhoa-16@users.noreply.github.com>
2024-10-20 12:45:44 +0200

    Create api.js

---
+++ src/sources/api.js
@@ -0,0 +1,19 @@
+const callToApi = () => {
+  
+  return fetch("https://hp-api.onrender.com/api/characters/") 
+    .then((response) => response.json())
+    .then((characters) => {
+      return characters.map((character) => ({
+        photo: character.image,
+        name: character.name,
+        species: character.species,
+        house: character.house,
+        id: character.id,
+        alive: character.alive,
+        gender: character.gender,
+        ancestry: character.ancestry,
+      })) 
+    });
+};
+
+export default callToApi;
