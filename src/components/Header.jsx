67d73a0e236ccfecadd212a9b72136294a64f3ba
16-Ainhoa-16 <117347227+16-Ainhoa-16@users.noreply.github.com>
2024-10-20 13:12:22 +0200

    Create Header.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import header from "../images/header.png"
+
+function Header() {
+  return (
+    <div className="header">
+          <img src={header} alt="harry potter" className="header__img"/>
+    </div>
+  )
+}
+
+export default Header;
